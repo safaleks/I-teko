f23645ba499c4c472ba21ab106ad86eae2f347c7
safaleks <safaleks@mail.ru>
2025-03-25 23:24:17 +0300

    правки по 5

--- src/Task5.java
+++ src/Task5.java
@@ -1,33 +1,28 @@
 public class Task5 {
     public static void main(String[] args) {
         enum Month {
-            JANUARY("Январь", 1),
-            FEBRUARY("Февраль", 2),
-            MARCH("Март", 3),
-            APRIL("Апрель", 4),
-            MAY("Май", 5),
-            JUNE("Июнь", 6),
-            JULY("Июль", 7),
-            AUGUST("Август", 8),
-            SEPTEMBER("Сентябрь", 9),
-            OCTOBER("Октябрь", 10),
-            NOVEMBER("Ноябрь", 11),
-            DECEMBER("Декабрь", 12);
+            JANUARY("Январь"),
+            FEBRUARY("Февраль"),
+            MARCH("Март"),
+            APRIL("Апрель"),
+            MAY("Май"),
+            JUNE("Июнь"),
+            JULY("Июль"),
+            AUGUST("Август"),
+            SEPTEMBER("Сентябрь"),
+            OCTOBER("Октябрь"),
+            NOVEMBER("Ноябрь"),
+            DECEMBER("Декабрь");
             private final String name;  // Название месяца (на русском)
-            private final int number;   // Номер месяца (1-12)
 
-            Month(String name, int number) {
+            Month(String name) {
                 this.name = name;
-                this.number = number;
             }
 
             public String getName() {
                 return name;
             }
 
-            public int getNumber() {
-                return number;
-            }
         }
 
         String monthName = "OCTOBER";
